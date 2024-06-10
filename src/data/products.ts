756fdf260318297e88e2e6c444b2c0bde1279d76
Daniel Santana <danielsantanachastel@gmail.com>
2024-06-10 19:46:43 -0300

    first commit

---
+++ src/data/products.ts
@@ -0,0 +1,193 @@
+import { Product } from "@/types/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        category: 'keyboard',
+        name: 'keyboard 1',
+        image: '/images/keyboard/keyboard1.png',
+        price: 49
+    },
+    {
+        id: 2,
+        category: 'keyboard',
+        name: 'Keyboard 2',
+        image: '/images/keyboard/keyboard2.png',
+        price: 49
+    },
+    {
+        id: 3,
+        category: 'keyboard',
+        name: 'Keyboard 3',
+        image: '/images/keyboard/keyboard3.png',
+        price: 49
+    },
+    {
+        id: 4,
+        category: 'keyboard',
+        name: 'Keyboard 4',
+        image: '/images/keyboard/keyboard4.png',
+        price: 49
+    },
+    {
+        id: 5,
+        category: 'keyboard',
+        name: 'Keyboard 5',
+        image: '/images/keyboard/keyboard5.png',
+        price: 49
+    },
+    {
+        id: 6,
+        category: 'keyboard',
+        name: 'Keyboard 6',
+        image: '/images/keyboard/keyboard6.png',
+        price: 49
+    },
+    {
+        id: 7,
+        category: 'keyboard',
+        name: 'Keyboard 7',
+        image: '/images/keyboard/keyboard7.png',
+        price: 49
+    },
+    {
+        id: 8,
+        category: 'keyboard',
+        name: 'Keyboard 8',
+        image: '/images/keyboard/keyboard8.png',
+        price: 49
+    },
+    {
+        id: 9,
+        category: 'keyboard',
+        name: 'Keyboard 9',
+        image: '/images/keyboard/keyboard9.png',
+        price: 49
+    },
+    {
+        id: 10,
+        category: 'keyboard',
+        name: 'Keyboard 10',
+        image: '/images/keyboard/keyboard10.png',
+        price: 49
+    },
+    {
+        id: 21,
+        category: 'mouse',
+        name: 'Mouse 1',
+        image: '/images/mouse/mouse1.png',
+        price: 81
+    },
+    {
+        id: 22,
+        category: 'mouse',
+        name: 'Mouse 2',
+        image: '/images/mouse/mouse2.png',
+        price: 81
+    },
+    {
+        id: 23,
+        category: 'mouse',
+        name: 'Mouse 3',
+        image: '/images/mouse/mouse3.png',
+        price: 81
+    },
+    {
+        id: 24,
+        category: 'mouse',
+        name: 'Mouse 4',
+        image: '/images/mouse/mouse4.png',
+        price: 81
+    },
+    {
+        id: 25,
+        category: 'mouse',
+        name: 'Mouse 5',
+        image: '/images/mouse/mouse5.png',
+        price: 81
+    },
+    {
+        id: 26,
+        category: 'mouse',
+        name: 'Mouse 6',
+        image: '/images/mouse/mouse6.png',
+        price: 81
+    },
+    {
+        id: 27,
+        category: 'mouse',
+        name: 'Mouse 7',
+        image: '/images/mouse/mouse7.png',
+        price: 81
+    },
+    {
+        id: 28,
+        category: 'mouse',
+        name: 'Mouse 8',
+        image: '/images/mouse/mouse8.png',
+        price: 81
+    },
+    {
+        id: 31,
+        category: 'monitor',
+        name: 'Monitor 1',
+        image: '/images/monitor/monitor1.png',
+        price: 49
+    },
+    {
+        id: 32,
+        category: 'monitor',
+        name: 'Monitor 2',
+        image: '/images/monitor/monitor2.png',
+        price: 49
+    },
+    {
+        id: 33,
+        category: 'monitor',
+        name: 'Monitor 3',
+        image: '/images/monitor/monitor3.png',
+        price: 49
+    },
+    {
+        id: 34,
+        category: 'monitor',
+        name: 'Monitor 4',
+        image: '/images/monitor/monitor4.png',
+        price: 49
+    },
+    {
+        id: 35,
+        category: 'monitor',
+        name: 'Monitor 5',
+        image: '/images/monitor/monitor5.png',
+        price: 49
+    },
+    {
+        id: 36,
+        category: 'monitor',
+        name: 'Monitor 6',
+        image: '/images/monitor/monitor6.png',
+        price: 49
+    },
+    {
+        id: 37,
+        category: 'monitor',
+        name: 'Monitor 7',
+        image: '/images/monitor/monitor7.png',
+        price: 49
+    },
+    {
+        id: 38,
+        category: 'monitor',
+        name: 'Monitor 8',
+        image: '/images/monitor/monitor8.png',
+        price: 49
+    },
+    {
+        id: 39,
+        category: 'monitor',
+        name: 'Monitor 9',
+        image: '/images/monitor/monitor9.png',
+        price: 49
+    },
+];
