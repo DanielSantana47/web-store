5e97f8a141d56824f2432644051f1da8a01c450e
Daniel Santana <135668123+DanielSantana47@users.noreply.github.com>
2024-06-27 19:05:12 +0000

    version 1.0

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "../ui/button"
 import { SiAlienware } from "react-icons/si";
 import {
@@ -5,17 +7,32 @@ import {
     SheetTrigger,
   } from "@/components/ui/sheet"
 import { Menu } from "./cart";
+import { useStore } from "../store/cart-store";
 
 export const Nav = ()=> {
+    const { cart } = useStore()
+
+    let cartElements = 0
+    for (let item of cart) {
+        cartElements += item.quantity
+    }
+
     return(
         <nav className="container fixed top-0 left-0 right-0 py-4 flex items-center justify-between bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
             <div className="flex items-center gap-4">
                 <h1 className="text-3xl">Web Store</h1>
             </div>
             <Sheet>
-                <SheetTrigger asChild>
-                    <Button className="flex gap-2 text-md"><SiAlienware className="text-lg"/> Cart</Button>
-                </SheetTrigger>
+                    <SheetTrigger asChild>
+                            <Button className="flex gap-2 text-md relative">
+                                <SiAlienware className="text-lg"/>
+                                {cartElements > 0 &&
+                                    <div className="w-4 h-4 flex items-center text-primary font-bold justify-center rounded-full bg-white absolute -top-1 -right-1 text-xs">{cartElements}</div>
+                                }
+                                Cart
+                            </Button>
+                    </SheetTrigger>
+                
                 <Menu/>
             </Sheet>
         </nav>
